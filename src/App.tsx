c7b181a20b3224dbe9cc936acc765c8b0e742c82
doolife <doolife@ncits.co.kr>
2023-12-28 10:26:45 +0900

    react typescript settings

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>Hello World</p>
+      </header>
+  );
+}
+
+export default App;
