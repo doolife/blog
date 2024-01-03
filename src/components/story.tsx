2e831932f43418de3d653e3fb4f791642b235d32
doolife <doolife@ncits.co.kr>
2024-01-03 12:41:35 +0900

    edit

--- src/components/story.tsx
+++ src/components/story.tsx
@@ -1,29 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
 
-// webpack의 require.context 대신 사용할 함수 정의
-const importAll = (r: any) => r.keys().map(r);
-
-// _posts 폴더 내의 모든 .md 파일 가져오기
-const markdownFiles = importAll((require as any).context('../_posts', false, /\.md$/));
 
 const Story = () => {
-    const [markdown, setMarkdown] = useState("");
-
-    useEffect(() => {
-        // 첫 번째 .md 파일 가져오기
-        const firstMarkdownFile = markdownFiles[0];
-
-        // 파일 읽기
-        fetch(firstMarkdownFile)
-            .then((response) => response.text())
-            .then((text) => setMarkdown(text));
-    }, []);
-
     return (
         <div className="story">
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+            My Story
         </div>
     );
 };
--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+// webpack의 require.context 대신 사용할 함수 정의
+const importAll = (r: any) => r.keys().map(r);
+console.log(importAll)
+
+// _posts 폴더 내의 모든 .md 파일 가져오기
+const markdownFiles = importAll((require as any).context('../posts', false, /\.md$/));
+
 const Work = () => {
+    const [markdown, setMarkdown] = useState("");
+
+    useEffect(() => {
+        // 첫 번째 .md 파일 가져오기
+        const firstMarkdownFile = markdownFiles[0];
+        // 파일 읽기
+        fetch(firstMarkdownFile)
+            .then((response) => response.text())
+            .then((text) => setMarkdown(text));
+    }, []);
+
     return (
         <div className="work">
-            <h1 className="work__tit">업무 경험</h1>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
         </div>
     );
 };
