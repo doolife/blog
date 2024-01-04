0e8956c8c93bd922cd8bec55009f6f8a957902b7
doolife <doolife@ncits.co.kr>
2024-01-04 15:32:03 +0900

    github pages 새로고침시 404 -> BrowserRouter에서 HashRouter로 변경 처리

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@
 import App from './App';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
