99d4caef694ffc2b724275e2fe5522882052af4b
doolife <doolife@ncits.co.kr>
2024-01-05 12:44:27 +0900

    eject test

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,16 +1,14 @@
-import {Buffer} from 'buffer';      // Webpack 5에서는 Node.js의 Buffer를 기본적으로 제공하지 않는다.(npm install buffer 설치)
-import matter from "gray-matter";   // YAML front matter를 추출하여 객체로 반환하는 라이브러리
+import fm from "front-matter";
 
 interface WorkProps {
-  propsMarkdown: string[];
+    propsMarkdown: string[];
 }
 
 const Main: React.FC<WorkProps> = ({ propsMarkdown }) => {
-    // front matter 추출한 전체 테이터 반환
-    const allFrontMatter = propsMarkdown.map((markdown) => {
-        const frontMatter = matter(Buffer.from(markdown));  // Buffer 사용하여 처리(없으면 gray-matter 에러)
-        return frontMatter.data;
-    });
+    console.log("propsMarkdown", propsMarkdown);
+
+    // front matter 추출한 전체 데이터 반환
+    const allFrontMatter = propsMarkdown.map((markdown) => fm(markdown).attributes);
 
     return (
         <div className="main">
@@ -19,21 +17,22 @@ const Main: React.FC<WorkProps> = ({ propsMarkdown }) => {
                 <a href="#." className="post-feed__list" key={index}>
                     <header
                         className="post-feed__list--image" 
-                        style={{ backgroundImage: `url(${key.image})` }}
+                        style={{ backgroundImage: `url(${(key as any).image})` }}
                     ></header>
 
                     <section
                         className="post-feed__list--title"
-                    >{key.title}</section>
+                    >{(key as any).title}</section>
+
 
                     <footer 
-                    className="post-feed__list--content"
-                    >{ key.content }</footer>
+                        className="post-feed__list--content"
+                    >{ (key as any).description }</footer>
                 </a>
             ))}
             </section>
         </div>
-      );
+    );
 };
 
 export default Main;
